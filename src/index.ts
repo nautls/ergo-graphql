835818faba96465a80bdaa17102a7214ee7b2a1c
SepehrGanji <sepehrganji79@gmail.com>
2022-05-21 19:52:58 +0430

    add query-complexity checking

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,17 @@ import "dotenv/config";
 import "reflect-metadata";
 import "./prototypes";
 
+import { getComplexity, simpleEstimator, fieldExtensionsEstimator } from "graphql-query-complexity";
 import GraphQLDatabaseLoader from "@mando75/typeorm-graphql-loader";
-import { ApolloServer } from "apollo-server";
+import { ApolloServer, ForbiddenError } from "apollo-server";
 import { initializeDataSource } from "./data-source";
 import { GraphQLSchema } from "graphql";
 import { DataSource } from "typeorm";
 import { generateSchema } from "./graphql/schema";
+import { BaseContext } from "apollo-server-types";
+import { GraphQLRequestListener } from "apollo-server-plugin-base";
+
+const MAX_COMPLEXITY_NUMBER = Number.parseInt(process.env.MAX_COMPLEXITY || "", 10) || 20;
 
 (async () => {
   const [dataSource, schema] = await Promise.all([initializeDataSource(), generateSchema()]);
@@ -17,6 +22,27 @@ import { generateSchema } from "./graphql/schema";
 async function startServer(schema: GraphQLSchema, dataSource: DataSource) {
   const server = new ApolloServer({
     schema,
+    plugins: [
+      {
+        requestDidStart: () => ({
+          didResolveOperation({ request, document }) {
+            const complexity = getComplexity({
+              schema,
+              operationName: request.operationName,
+              query: document,
+              variables: request.variables,
+              estimators: [
+                fieldExtensionsEstimator(),
+                simpleEstimator({ defaultComplexity: 1 }),
+              ],
+            });
+            if(complexity > MAX_COMPLEXITY_NUMBER) {
+              throw new ForbiddenError('Query is too complex!');
+            }
+          },
+        }),
+      },
+    ],
     csrfPrevention: true,
     context: { loader: new GraphQLDatabaseLoader(dataSource) }
   });
