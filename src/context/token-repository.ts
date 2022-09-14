cc57a9fbe24d1b812befbe32761cf5caca3574e2
SepehrGanji <sepehrganji79@gmail.com>
2022-09-14 09:02:40 +0430

    refactor batch tokenId

--- src/context/token-repository.ts
+++ src/context/token-repository.ts
@@ -16,6 +16,11 @@ export class TokenRepository extends BaseRepository<TokenEntity> {
   public override async find(options: TokenFindOptions): Promise<TokenEntity[]> {
     const { tokenIds } = options;
     return this.findBase(options, (filterQuery) => {
+      if (options.where?.tokenId && tokenIds) {
+        tokenIds.push(options.where.tokenId);
+        delete options.where.tokenId;
+      }
+
       if (tokenIds) {
         filterQuery = filterQuery.andWhere(`${this.alias}.tokenId IN (:...tokenIds)`, {
           tokenIds
--- src/graphql/resolvers/token-resolver.ts
+++ src/graphql/resolvers/token-resolver.ts
@@ -4,10 +4,16 @@ import { Token } from "../objects";
 import { removeUndefined } from "../../utils";
 import { PaginationArguments } from "./pagination-arguments";
 import { GraphQLContext } from "../context-type";
+import { ArrayMaxSize } from "class-validator";
 
 @ArgsType()
 class TokensQueryArgs {
+  /** @deprecated */
+  @Field(() => String, { nullable: true })
+  tokenId?: string;
+
   @Field(() => [String], { nullable: true })
+  @ArrayMaxSize(20)
   tokenIds?: [string];
 
   @Field(() => String, { nullable: true })
@@ -21,14 +27,14 @@ class TokensQueryArgs {
 export class TokenResolver {
   @Query(() => [Token])
   async tokens(
-    @Args() { tokenIds, boxId, name }: TokensQueryArgs,
+    @Args() { tokenId, tokenIds, boxId, name }: TokensQueryArgs,
     @Args({ validate: true }) { skip, take }: PaginationArguments,
     @Ctx() context: GraphQLContext,
     @Info() info: GraphQLResolveInfo
   ) {
     return context.repository.tokens.find({
       resolverInfo: info,
-      where: removeUndefined({ boxId, name }),
+      where: removeUndefined({ boxId, name, tokenId }),
       tokenIds,
       skip,
       take
