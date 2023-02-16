fd898b312f518256de978ccf8c485d90c0f894b3
capt-nemo429 <nemo429@protonmail.com>
2023-02-16 20:41:49 -0300

    update types

--- packages/ts-types/src/index.ts
+++ packages/ts-types/src/index.ts
@@ -11,7 +11,7 @@ export type Scalars = {
   Int: number;
   Float: number;
   /** The `JSONObject` scalar type represents JSON objects as specified by [ECMA-404](http://www.ecma-international.org/publications/files/ECMA-ST/ECMA-404.pdf). */
-  JSONObject: unknown;
+  JSONObject: any;
 };
 
 export type AdProof = {
