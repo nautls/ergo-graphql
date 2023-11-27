09e0c4d22e4bf7451713aa5814d20ef9bced3567
SepehrGanji <sepehrganji79@gmail.com>
2023-11-26 22:50:31 -0700

    Complete node-service tests

--- tests/services/node-service.spec.ts
+++ tests/services/node-service.spec.ts
@@ -1,11 +1,51 @@
+import axios from "axios";
 import { nodeService } from '../../src/services/node-service';
 
 jest.mock('axios');
 
 describe('NodeService', () => {
-  it('Should workd', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  /**
+   * @description Test if the checkUrl runs without throwing an error
+   * @expected Should not throw an error
+   */
+  it('Should not return error while checking URL', () => {
     expect(() => {
       nodeService.checkUrl();
     }).not.toThrowError();
   });
+
+  /**
+   * @description Run getNodeInfo to feth the info from the node
+   * @expected Should return nodeInfo
+   */
+  it('Should return nodeInfo', async() => {
+    axios.get = jest.fn().mockResolvedValue({ nodeInfo: 1 });
+    const nodeInfo = await nodeService.getNodeInfo();
+    expect(nodeInfo).toEqual({ nodeInfo: 1 });
+  });
+
+  /**
+   * @description Run checkTransaction to check the transaction
+   * @expected Should return transaction
+   */
+  it('Should return transaction', async() => {
+    axios.post = jest.fn().mockResolvedValue({ transaction: 1 });
+    const transaction = await nodeService.checkTransaction(null as any);
+    expect(transaction).toEqual({ transaction: 1 });
+  });
+
+  /**
+   * @description Run submitTransaction to submit the transaction
+   * @expected Should return transaction
+   */ 
+  it('Should return transaction', async() => {
+    axios.post = jest.fn().mockResolvedValue({ transaction: 1 });
+    const transaction = await nodeService.submitTransaction(null as any);
+    expect(transaction).toEqual({ transaction: 1 });
+  });
+
 });
